e8f7763371e3ad9412857cd9094c59290b7f95b6
Noah Torrance <noahtorrance27@gmail.com>
2022-02-22 14:09:45 +0000

    Replace battery with script

--- config.def.h
+++ config.def.h
@@ -67,17 +67,17 @@ static const struct arg args[] = {
 	/* function 		format			argument */
 
 	/* network download speed | run "ip link" to view network interfaces and replace "enp39s0" with yours */
-	{ netspeed_rx, 		"  %sB/s  ",		"enp39s0" },
+	{ netspeed_rx, 		"  %sB/s  ",		"wlan0" },
 	/* network upload speed */
-//	{ netspeed_tx, 		" %sB/s  ",		"enp39s0" },
+//	{ netspeed_tx, 		" %sB/s  ",		"wlan0" },
 	/* cpu usage */
 	{ cpu_perc,		" %s%%  ",		NULL },
 	/* ram usage */
 	{ ram_perc,		" %s%%  ",		NULL },
 	/* cpu temperature */
 //	{ run_command,		" %s°  ",		"sensors | awk 'FNR == 3 {print $2}' | cut -c 2-3" },
-	/* battery charge */
-//	{ battery_perc,		" %s%%  ",		"BAT0" },
+	/* battery level */
+//	{ run_command,		" %s  ",		"if (( $(cat /sys/class/power_supply/BAT0/capacity) > 5 )); then bat=$(cat /sys/class/power_supply/BAT0/capacity); echo $bat'%'; else if [[ $(($(date +%s) % 2 )) == 0 ]]; then bat=$(cat /sys/class/power_supply/BAT0/capacity); echo $bat'%'; else echo 'LOW'; fi; fi" },
 	/* volume level */
 //	{ run_command,		" %s%%  ",		"amixer get Master | awk 'FNR == 5 {print $4}' | tr -d '[]%'" },
 	/* day, date, month, time */
