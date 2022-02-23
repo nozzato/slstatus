a3881d46c0a20af20f76b4b60beb44cda7ae7219
Noah Torrance <noahtorrance27@gmail.com>
2022-02-23 17:48:38 +0000

    Update volume applet

--- applets/volume_applet.sh
+++ applets/volume_applet.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
 
-vol=$(amixer -D pulse sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }')
-echo $vol
+vol=$(amixer -D pulse sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }' | sed 's/%//')
+
+if (($vol == 0)); then
+    echo "  $vol%"
+elif (($vol > 0 && $vol < 10)); then
+    echo "  $vol%"
+elif (($vol >= 10 && $vol < 50)); then
+    echo " $vol%"
+elif (($vol >= 50)); then
+    echo " $vol%"
+fi
+
