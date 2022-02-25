a5dfd70ee8c6240fb0a5f3d87029c0547385737f
Noah Torrance <noahtorrance27@gmail.com>
2022-02-25 11:47:12 +0000

    Update temperature script

--- scripts/temperature_script.sh
+++ scripts/temperature_script.sh
@@ -2,9 +2,7 @@
 
 temp=$(sensors | awk 'FNR == 3 {print $2}' | cut -c 2-3)
 
-if (( $temp > 85 )); then
-    echo " $temp"
-elif (( $temp >= 80 && $temp <= 85 )); then
+if (( $temp >= 80 && $temp <= 85 )); then
     echo " $temp"
 elif (( $temp >= 70 && $temp < 80 )); then
     echo " $temp"
@@ -14,8 +12,8 @@ elif (( $temp >= 50 && $temp < 60 )); then
     echo " $temp"
 elif (( $temp >= 40 && $temp < 50 )); then
     echo " $temp"
-elif (( $temp >= 30 && $temp < 40 )); then
+elif (( $temp >= 0 && $temp < 40 )); then
     echo " $temp"
 else
-    echo " $temp"
+    echo " $temp"
 fi
