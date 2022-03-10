0f0bb420e3d9a2ed505463f706090da9bd9ee90e
Noah Torrance <noahtorrance27@gmail.com>
2022-03-10 13:44:15 +0000

    Update temperature script

--- scripts/temperature_script.sh
+++ scripts/temperature_script.sh
@@ -1,20 +1,24 @@
 #! /bin/bash
 
-#temp=$(sensors | awk 'FNR == 3 {print $2}' | cut -c 2-3)
-temp=$(sensors | awk 'FNR == 17 {print $2}' | cut -c 2-3)
+temp=$(sensors | awk 'FNR == 7 {print $2}' | cut -c 2-3)
 
-if (( $temp >= 80 && $temp <= 85 )); then
-    echo " 0$temp"
-elif (( $temp >= 70 && $temp < 80 )); then
-    echo " 0$temp"
-elif (( $temp >= 60 && $temp < 70 )); then
-    echo " 0$temp"
-elif (( $temp >= 50 && $temp < 60 )); then
-    echo " 0$temp"
+if (( $temp == 0 )); then
+    echo " ZRO"
+elif (( $temp > 0 && $temp < 40 )); then
+    echo " 0$temp"
 elif (( $temp >= 40 && $temp < 50 )); then
     echo " 0$temp"
-elif (( $temp >= 0 && $temp < 40 )); then
-    echo " 0$temp"
+elif (( $temp >= 50 && $temp < 60 )); then
+    echo " 0$temp"
+elif (( $temp >= 60 && $temp < 70 )); then
+    echo " 0$temp"
+elif (( $temp >= 70 && $temp <= 100 )); then
+    echo " 0$temp"
 else
-    echo " 0$temp"
+    if (( $(echo $temp | grep "^-\?[0-9]+$") != "" )); then
+        echo " OoR"
+    else
+        echo " NaN"
+    fi
 fi
+
